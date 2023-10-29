03dc880336a46d02da7f41d1901d1aa058bdf858
Yashar Honarmandi <yashar.honarmandi@entecon.se>
2023-10-29 20:15:18 +0100

    Add function for performing requests

--- endpointWrappingClasses.py
+++ endpointWrappingClasses.py
@@ -1,14 +1,30 @@
 import requests
 
 class dataEndpointWrapper:
-    def __init__(self, url, method, body, parameters):
-        self.url = url
-        self.method = method
-        self.body = body
-        self.parameters = parameters
+    def __init__(self):
+        pass
 
     def getData(self):
         pass
+
+    def performWebRequest(self, url, method, body, parameters):
+        if method == "GET":
+            return requests.get(url)
+        elif method == "POST":
+            # return requests.post(url, body=body, data=body)
+            pass
+        elif method == "PUT":
+            # requests.put(url)
+            pass
+        elif method == "DELETE":
+            # return requests.delete(url)
+            pass
+        elif method == "HEAD":
+            # return requests.head(url)
+            pass
+        elif method == "OPTIONS":
+            # return requests.options('https://httpbin.org/get')
+            pass
 
 class dataEndpointResponse:
     def __init__(self, success, data, message):
